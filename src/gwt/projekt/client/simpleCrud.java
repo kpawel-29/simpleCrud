4b4c1ed44243d66d39052ac9db1392edd52a2c7c
kpawel-29 <pakadlubowski@gmail.com>
2015-04-28 23:47:34 +0200

    first commit

---
+++ src/gwt/projekt/client/simpleCrud.java
@@ -0,0 +1,13 @@
+package gwt.projekt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class simpleCrud implements EntryPoint{
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
